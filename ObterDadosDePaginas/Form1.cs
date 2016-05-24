48a1dabf9c2ed42af06e38f6db7273a093cb60f2
Rafael Prado <rafael_adcp@hotmail.com>
2016-05-24 19:02:00 -0300

    no message

---
+++ ObterDadosDePaginas/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObterDadosDePaginas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Visible = false;
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            button1.Visible = true;
+            //após terminar de atualizar a página ele tenta buscar os dados novamente
+            obterDados();
+        }
+
+        public void obterDados()
+        {
+
+            try
+            {
+                var possiveis = webBrowser1.Document.GetElementsByTagName("span");
+                richTextBox1.Text = "";
+                foreach (HtmlElement doc in possiveis)
+                {
+                    richTextBox1.Text += doc.OuterText + "\n";
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh(WebBrowserRefreshOption.Completely);
+            webBrowser1.Navigate(webBrowser1.Url);
+        }
+    }
+}
+
+
